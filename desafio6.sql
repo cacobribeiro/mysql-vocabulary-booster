6685ad95eff5b050008f6333d782161481972acc
Caio Ribeiro <cacoribeiro@pop-os.localdomain>
2020-08-25 18:01:26 -0300

    Desafio 6

---
+++ desafio6.sql
@@ -0,0 +1,17 @@
+SELECT CONCAT(FIRST_NAME, ' ', LAST_NAME) AS `Nome completo`,
+    (
+        SELECT JOB_TITLE
+        FROM jobs
+        WHERE employees.JOB_ID = jobs.JOB_ID
+    ) AS Cargo,
+    (
+        SELECT HIRE_DATE
+    ) AS `Data de início do cargo`,
+    (
+        SELECT DEPARTMENT_NAME
+        FROM departments
+        WHERE employees.DEPARTMENT_ID = DEPARTMENT_ID
+    ) AS "Departamento"
+FROM employees
+ORDER BY `Nome completo` DESC,
+    Cargo;
