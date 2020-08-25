8df54568920fd7af574f450b6ed9e459d7d20f86
Caio Ribeiro <cacoribeiro@pop-os.localdomain>
2020-08-25 17:17:55 -0300

    Desafio 6

---
+++ desafio5.sql
@@ -0,0 +1,6 @@
+SELECT JOB_TITLE AS Cargo,
+    (MAX_SALARY - MIN_SALARY) AS `Variação Salarial`,
+    ROUND((MIN_SALARY / 12), 2) AS `Média mínima mensal`,
+    ROUND((MAX_SALARY / 12), 2) AS `Média máxima mensal`
+FROM jobs
+ORDER BY `Variação Salarial`, Cargo;
