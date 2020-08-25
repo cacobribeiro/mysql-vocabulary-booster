d083dcfad2cf467af664cae486f70ac87d20e55a
Caio Ribeiro <cacoribeiro@pop-os.localdomain>
2020-08-25 18:43:16 -0300

    Desafio 6

--- desafio6.sql
+++ desafio6.sql
@@ -1,17 +1,20 @@
-SELECT CONCAT(FIRST_NAME, ' ', LAST_NAME) AS `Nome completo`,
+SELECT (
+        SELECT CONCAT(FIRST_NAME, ' ', LAST_NAME)
+        FROM employees
+        WHERE EMPLOYEE_ID = X.EMPLOYEE_ID
+    ) AS `Nome completo`,
     (
         SELECT JOB_TITLE
         FROM jobs
-        WHERE employees.JOB_ID = jobs.JOB_ID
+        WHERE JOB_ID = X.JOB_ID
     ) AS Cargo,
-    (
-        SELECT HIRE_DATE
-    ) AS `Data de início do cargo`,
+    START_DATE AS `Data de início do cargo`,
     (
         SELECT DEPARTMENT_NAME
         FROM departments
-        WHERE employees.DEPARTMENT_ID = DEPARTMENT_ID
+        WHERE DEPARTMENT_ID = X.DEPARTMENT_ID
     ) AS "Departamento"
-FROM employees
+from job_history AS X
+WHERE EMPLOYEE_ID <> 0
 ORDER BY `Nome completo` DESC,
     Cargo ASC;
