d0748992fc0598f40739e3ad9c8d5cde169fae71
Caio Ribeiro <cacoribeiro@pop-os.localdomain>
2020-08-25 18:05:18 -0300

    Fix 1, 4

--- desafio1.sql
+++ desafio1.sql
@@ -1,4 +1,4 @@
 SELECT COUNTRY_NAME AS País,
-    IF(REGION_ID = 1, 'incluido', 'não incluido') AS 'Status Inclusão'
+    IF(REGION_ID = 1, 'incluído', 'não incluído') AS 'Status Inclusão'
 FROM hr.countries
 ORDER BY País;
--- desafio4.sql
+++ desafio4.sql
@@ -3,13 +3,14 @@ SELECT (
         FROM hr.jobs
         WHERE JOB_ID = employees.JOB_ID
     ) AS Cargo,
-    ROUND(AVG(SALARY), 2) AS `Média Salarial`,
+    ROUND(AVG(SALARY), 2) AS `Média salarial`,
     CASE
         WHEN ROUND(AVG(SALARY), 2) BETWEEN 2000 AND 5800 THEN 'Júnior'
         WHEN ROUND(AVG(SALARY), 2) BETWEEN 5801 AND 7500 THEN 'Pleno'
-        WHEN ROUND(AVG(SALARY), 2) BETWEEN 7501 AND 10500 THEN 'Senior'
+        WHEN ROUND(AVG(SALARY), 2) BETWEEN 7501 AND 10500 THEN 'Sênior'
         WHEN ROUND(AVG(SALARY), 2) > 10500 THEN 'CEO'
     END AS 'Senioridade'
 FROM hr.employees
 GROUP BY Cargo
-ORDER BY `Média Salarial`, Cargo;
+ORDER BY `Média Salarial`,
+    Cargo;
