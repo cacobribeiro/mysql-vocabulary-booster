1cf076e49226493ea11cd5afb1af7be4bcd1d91d
Caio Ribeiro <cacoribeiro@pop-os.localdomain>
2020-08-26 17:10:43 -0300

    Desafio 7

--- desafio7.sql
+++ desafio7.sql
@@ -1,8 +1,27 @@
-SELECT UCASE(CONCAT(FIRST_NAME, ' ', LAST_NAME)) AS `Nome completo`,
-    JOB.START_DATE AS 'Data de início',
-    EM.SALARY AS 'Salário'
-FROM employees AS EM,
-job_history AS JOB
-WHERE JOB.EMPLOYEE_ID = EM.EMPLOYEE_ID
-ORDER BY `Nome completo`,
-    EM.HIRE_DATE;
+SELECT
+    (
+        SELECT
+            UPPER(CONCAT(FIRST_NAME, ' ', LAST_NAME))
+        FROM
+            hr.employees
+        WHERE
+            employees.EMPLOYEE_ID = job_history.EMPLOYEE_ID
+    ) AS `Nome completo`,
+    START_DATE AS `Data de início`,
+    (
+        SELECT
+            SALARY
+        FROM
+            hr.employees
+        WHERE
+            employees.EMPLOYEE_ID = job_history.EMPLOYEE_ID
+    ) AS `Salário`
+FROM
+    hr.job_history
+WHERE
+    MONTH(START_DATE) = 1
+    OR MONTH(START_DATE) = 2
+    OR MONTH(START_DATE) = 3
+ORDER BY
+    `Nome completo`,
+    `Data de início`;
